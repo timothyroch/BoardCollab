3d38ce2f74c0468a82eb259cda95f0c6b830efaf
timothyroch <your-email@example.com>
2025-07-06 21:51:37 -0400

    feat: setup WebSocket dependencies and generate tasks gateway

--- backend/src/tasks/tasks.module.ts
+++ backend/src/tasks/tasks.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TasksController } from './tasks.controller';
+import { TasksGateway } from './tasks.gateway';
 
 @Module({
-    controllers: [TasksController]
+    controllers: [TasksController],
+    providers: [TasksGateway]
 })
 export class TasksModule {}
